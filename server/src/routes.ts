2753b9562268e95d84decb0796d2928ea3959948
breno-do-val <brenorvale@gmail.com>
2020-06-04 06:43:33 -0300

    Creating database

---
+++ server/src/routes.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import knex from './database/connection';
+
+const routes = express.Router();
+
+routes.get('/items', async (req, res) => {
+    const items = await knex('items').select('*');
+
+    const serializedItems = items.map(item => {
+        return {
+            title: item.title,
+            image_url: `http:localhost:3333/uploads/${item.image}`,
+        };
+    });
+    return res.json(serializedItems);
+});
+
+export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,12 @@
 import express from 'express';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-    console.log("Listagem de usuários!");
+app.use(express.json());
+app.use(routes);
 
-    res.json([
-        "Diego",
-        "Cleiton",
-        "Robson"
-    ]);
-});
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(3333);
